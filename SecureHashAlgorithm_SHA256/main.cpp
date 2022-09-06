967ef26aced6c442cca1d60b6e0b544af9507fd1
Anthony Kuregian <AnthonyKuregian@yahoo.com>
2022-09-06 11:33:05 -0700

    Save I can reset from

--- SecureHashAlgorithm_SHA256/main.cpp
+++ SecureHashAlgorithm_SHA256/main.cpp
@@ -13,12 +13,7 @@ int main()
 //	std::string input = "abcdefghijklmnopqrstuvwrxyz"; // 21B9144A8F751450CBEBFC02B7797AD0E688DBF884EAEAF80044EC1FCB2470A1 // WORKS
 //	std::string input = "hello world Good Morning, adding some more to this to fi"; // 19f8b22aaf4b0dba87ffcfa65d55d3e2a0870b23c6ee9ccc796c91f77554d9e1 // DOESNT WORK
 	//std::string input = "hello world Good Morning, This is a longer messagge than before to add bits to the second chunk"; //9E7574E019A7FB33B516772F9D5E854DDFC60004B36FF3FF9562B07326D2573F // DOESNT WORK
-
-	//std::string input = "ck";
-	bool flipit = false;
-	std::string input;
-	if(flip(flipit)) { input = "abcdefghijklmnopqrstuvwrxyz"; }
-	else { input = "hello world Good Morning, adding some more to this to fi asdasdasasasdfkjasdkl;fj;lkjads;lfjkadslkjadslf;kjagoijro;ijfo;iajdfioajdfiojioajsdiofjoiajiodsgjoaidsjgoijfg;oiajfdg;oijsfd;ogijSDOPIJGSDFIOJGSLIUDFHGoijOIJA;OSDFIJGS;OFDIJGS;OIFDJGP;OIAJRFG;OSZIJDFG;OISJAFDG;OIJSDFG;OIJDFGIJAOIJ"; }
+	std::string input = "hello world Good Morning, adding some more to this to fi asdasdasasasdfkjasdkl;fj;lkjads;lfjkadslkjadslf;kjagoijro;ijfo;iajdfioajdfiojioajsdiofjoiajiodsgjoaidsjgoijfg;oiajfdg;oijsfd;ogijSDOPIJGSDFIOJGSLIUDFHGoijOIJA;OSDFIJGS;OFDIJGS;OIFDJGP;OIAJRFG;OSZIJDFG;OISJAFDG;OIJSDFG;OIJDFGIJAOIJ"; 
 
 	SHA_256 sha256(input);
 	std::cin.get();
